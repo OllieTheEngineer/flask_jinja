ddbaf235434968ee1ba576ab115141267381b1f8
ollie <olivinanzali07@gmail.com>
2022-04-28 14:12:47 -0400

    added updated app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, request, render_template
+from flask import Flask, render_template, request
+from random import randint
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = "Godzchild7"
+app.debug = True
 debug = DebugToolbarExtension(app)
 
 
@@ -20,6 +22,21 @@ def home_page():
     </html>
     """
     return html
+
+
+@app.route('/form')
+def show_form():
+  return render_template("form.html")
+
+@app.route('/greet')
+def get_greeting():
+  username = request.args["username"]
+  return render_template("greet.html", username=username)
+
+@app.route('/lucky')
+def lucky_number():
+  num = randint(1, 20)
+  return render_template('lucky.html', lucky_num=num, msg="You are so very lucky!")
 
 
 @app.route('/hello')
