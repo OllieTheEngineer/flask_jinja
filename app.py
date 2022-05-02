61af480fb045bf8f91dc1eea33ff21ada010e1d6
ollie <olivinanzali07@gmail.com>
2022-05-02 18:43:34 -0400

    updated app.py

--- app.py
+++ app.py
@@ -1,43 +1,81 @@
-from flask import Flask, render_template, request
-from random import randint
+from flask import Flask, render_template, request, redirect
+from random import randint, choice, sample
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = "Godzchild7"
 app.debug = True
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
+compliments = ["Cool", "Funny", "exuberant", "beautiful", "smart"]
+
+movies = ['Pretty Woman', 'Lean On Me', 'Sister Act II']
 
 @app.route('/')
 def home_page():
-    """Shows home page"""
-    html = """ 
-    <html>
-      <body>
-        <h1>Home Page</h1>
-        <p>Welcome to my simple app!</p>
-        <a href='/hello'>Go to hello page</a>
-      </body>
-    </html>
-    """
-    return html
+    # """Shows home page"""
+    # html = """ 
+    # <html>
+    #   <body>
+    #     <h1>Home Page</h1>
+    #     <p>Welcome to my simple app!</p>
+    #     <a href='/hello'>Go to hello page</a>
+    #   </body>
+    # </html>
+    # """
+    return render_template("home.html")
+
+@app.route('/old-home-page')
+def redirect_to_home():
+  """ redirect to the home page"""
+  return redirect("/")
+
+
+@app.route('/movies-1')
+def get_ollie():
+  return render_template('movies1.html', movies=movies)
+
+
+app.route('/moveez/new', methods=["POST"])
+def add_movies():
+  """ADD movies in DB"""
+  title = request.form['title']
+    # pretending adding to DB
+  movies.append(title)
+  return redirect('/movies-1')
 
 
 @app.route('/form')
 def show_form():
   return render_template("form.html")
 
+@app.route('/form-2')
+def show_form2():
+  return render_template("form_2.html")
+
 @app.route('/greet')
 def get_greeting():
   username = request.args["username"]
-  return render_template("greet.html", username=username)
+  nice_thing = choice(compliments)
+  return render_template("greet.html", username=username, compliment=nice_thing)
+
+@app.route('/greet-2')
+def get_greeting_2():
+  username = request.args["username"]
+  wants = request.args.get("wants_compliments")
+  nice_things = sample(compliments, 3)
+  return render_template("greet_2.html", username=username, wants_compliments=wants, compliments=nice_things)
 
 @app.route('/lucky')
 def lucky_number():
-  num = randint(1, 20)
+  num = randint(1, 10)
   return render_template('lucky.html', lucky_num=num, msg="You are so very lucky!")
 
+@app.route('/spell/<word>')
+def spell_word(word):
+  return render_template('spell_word.html', word=word)
 
 @app.route('/hello')
 def say_hello():
@@ -48,7 +86,7 @@ def say_hello():
 @app.route('/goodbye')
 def say_bye():
     """Says good bye"""
-    return "GOOD BYE!!!"
+    return render_template("goodbye.html")
 
 
 @app.route('/search')
